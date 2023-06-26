66dd6c9a306f628c0c3e32c9f7948cf1dfd65e0f
Anericokakai <challykiddoh@gmail.com>
2023-06-26 18:59:55 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 import axios from "axios";
 
-import Express from "express";
+import express from "express";
 import generate_Time_stamp from "./helper.js";
 import cors from "cors";
 // ! app configuration
 
-const app = Express();
+const app = express();
 dotenv.config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,66 +16,67 @@ app.use(bodyParser.json());
 // !call  time stamp helper
 const Timestamp = generate_Time_stamp();
 
+
 // !generate password from short code passkey and  and time stamp
 
-const password = new Buffer.from(
+const password = btoa(
   process.env.SHORT_CODE + process.env.PASS_KEY + Timestamp
-).toString("base64");
+);
 
 // ! authentication is base 64 encoding of secretkey and consumer key
 
-const authentication = new Buffer.from(
+const authentication = btoa(
   `${process.env.CONSUMER_KEY}:${process.env.CONSUMER_SECRETE}`
 );
+console.log(authentication);
+
 // !middle ware to geenarete the token
-let token;
+
 const Token_Gnerator = async (req, res, next) => {
-  await axios
-    .get(
-      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
-      {
-        headers: {
-          Authorization:`Basic ${authentication}`,
-        },
-      }
-    )
-    .then((results) => {
-      console.log(results);
-      // token = results.data.access_token;
-      // !call the next
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  await axios .get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',{
+    headers:{
+      Authorization:`Basic ${authentication}`
+    }
+  }).then(data=>{
+    res.Authorization=data.data
+  })
+  next();
 };
 // !listen to the server
 
 
-app.post('/api',Token_Gnerator,(req,res)=>{
+app.post('/callback',(req,res)=>{
 
-
+  console.log(req.body)
+ 
 })
+app.post("/home",Token_Gnerator, (req, res) => {
+  res.json({result:res.Authorization });
 
-app.post("/api/messpayment/mpesa", async (req, res) => {
+
+});
+
+app.post("/api/messpayment/mpesa",Token_Gnerator, async (req, res) => {
   // !user information
   const { phone, amount } = req.body;
+  const token=res.Authorization.access_token
+  console.log(token)
   axios
     .post(
       "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
 
       {
-        BusinessShortCode: process.env.SHORT_CODE,
-        Password: password,
-        Timestamp: Timestamp,
-        TransactionType: "CustomerPayBillOnline",
-        Amount: 1,
-        PartyA: 254792626899,
-        PartyB: process.env.SHORT_CODE,
-        PhoneNumber: 254792626899,
-        CallBackURL: "https://mydomain.com/b2c/result",
-        AccountReference: 254792626899,
-        transactionDesc: "testing api",
+        "BusinessShortCode": process.env.SHORT_CODE,    
+   "Password": password,    
+   "Timestamp":Timestamp,    
+   "TransactionType": "CustomerPayBillOnline",    
+   "Amount": "1",    
+   "PartyA":254792626899,    
+   "PartyB":process.env.SHORT_CODE,    
+   "PhoneNumber":254792626899,    
+   "CallBackURL": "https://239b-41-89-227-171.ngrok-free.app/callback",    
+   "AccountReference":"Test",    
+   "TransactionDesc":"Test",
       },
       //   header
       {
@@ -84,9 +85,9 @@ app.post("/api/messpayment/mpesa", async (req, res) => {
         },
       }
     )
-    .then((res) => {
-      console.log(data);
-      res.json(data);
+    .then((results) => {
+      console.log(results.data);
+      res.json(results.data);
     })
     .catch((err) => {
       console.log(err);
